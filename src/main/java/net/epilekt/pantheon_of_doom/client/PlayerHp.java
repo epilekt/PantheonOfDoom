c6bcb5db56069a5fd1216e56714e3d1c72137e1b
epilekt <dmnickdol@vk.com>
2024-01-02 18:21:36 +0600

    health bar issue

--- src/main/java/net/epilekt/pantheon_of_doom/client/PlayerHp.java
+++ src/main/java/net/epilekt/pantheon_of_doom/client/PlayerHp.java
@@ -6,78 +6,56 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.client.gui.overlay.IGuiOverlay;
-
-import static net.minecraft.world.entity.ai.attributes.Attributes.MAX_HEALTH; //??
+import net.minecraft.ChatFormatting;
+import static net.minecraft.world.entity.ai.attributes.Attributes.MAX_HEALTH;
 
 public class PlayerHp {
-
-    //public static void modifyPlayerHealth(LivingEntity player, float newMaxHealth) {
-    //    player.getAttribute(MAX_HEALTH).getModifiers().forEach(attributeModifier -> {
-    //        if (attributeModifier.getName().equals("InitialHealth")) {
-    //            player.getAttribute(MAX_HEALTH).removeModifier(attributeModifier);
-    //        }
-    //    });
-    //    player.getAttribute(MAX_HEALTH).addPermanentModifier(new AttributeModifier("InitialHealth", 100f, AttributeModifier.Operation.ADDITION));
-    //    player.setHealth(100f);
-    //}
-
     public static final ResourceLocation BASE = new ResourceLocation(PantheonOfDoom.MOD_ID,
-            "textures/gui/overlays/base_bar.png");
+            "textures/gui/overlays/back_bar.png");
     public static final ResourceLocation HP_POINTS = new ResourceLocation(PantheonOfDoom.MOD_ID,
-            "textures/gui/overlays/test.png");
+            "textures/gui/overlays/hp_point.png"); //need better texture, maybe darker
+    public static final IGuiOverlay BASE_BAR = ((gui, poseStack, partialTick, width, height) -> {
+        int x = width / 2 - 242;
+        int y = height + 8;
 
-    public static final IGuiOverlay BASE_HUD = ((gui, poseStack, partialTick, width, height) -> {
-        int x = width / 2 - 91;
-        int y = height + 9;
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderColor(1f,1f,1f,1f);
         RenderSystem.setShaderTexture(0, BASE);
-
-
-
-        GuiComponent.blit(poseStack, x, y - 54, 0, 0, 100, 16, 100, 16);
-
-
-
+        //----------------------screen x and y coordinates----instant pixel x,y------------sprite width--------------image width----
+        GuiComponent.blit(poseStack, x, y - 54, 0, 0, 108, 16, 108, 16);
     });
-
     public static final IGuiOverlay HEALTH_HUD = ((gui, poseStack, partialTick, width, height) -> {
-            int x = width / 2 - 91;
-            int y = height + 9;
-
-            RenderSystem.setShader(GameRenderer::getPositionTexShader);
-            RenderSystem.setShaderColor(1f,1f,1f,1f);
-            RenderSystem.setShaderTexture(0, HP_POINTS);
+        int x = width / 2 - 238;
+        int y = height + 12;
 
         Player player = Minecraft.getInstance().player;
+        int player_health = (int) player.getAttribute(MAX_HEALTH).getBaseValue();
+        int current_player_health = (int) player.getHealth();
+        String healthString = current_player_health + "/" + player_health;
+        int TEXT_COLOR = ChatFormatting.WHITE.getColor();
+        gui.getFont().draw(poseStack, healthString, x, y, TEXT_COLOR);//just fuckin text x/20 hp, suka blyat, why doesnt work
+
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderColor(1f,1f,1f,1f);
+        RenderSystem.setShaderTexture(0, HP_POINTS);
         if (player != null) {
-            int player_health = (int) player.getAttribute(MAX_HEALTH).getBaseValue();
-            int current_player_health = (int) player.getHealth();
-            for (int i = 0; i < 10; i++) {
+            int fullSegments = current_player_health;
+            for (int i = 0; i < 20; i++) { //in future add 100 sprites? for 100 hp and more and smoothest animation
                 int spriteX = 0;
-                int spriteY = 3;
-                int spriteWidth = 15;
-                int spriteHeight = 12;
-                float spacing = 0.1f;
-                float totalWidth = (spriteWidth + spacing) * 10;
-                if (current_player_health > i * (player_health / 10)) {
-                    spriteX = 15;
+                int spriteY = 0;
+                int spriteWidth = 5;
+                int spriteHeight = 8;
+                float xPos = x + i * spriteWidth;
+                if (i < fullSegments) { //full sprite
+                    GuiComponent.blit(poseStack, (int) xPos, y - 54, spriteX, spriteY, spriteWidth, spriteHeight, 10, 8);
+                } else { //empty sprite
+                    GuiComponent.blit(poseStack, (int) xPos, y, spriteX + spriteWidth, spriteY, spriteWidth, spriteHeight, 10, 8);
                 }
-                float xPos = x + i * (spriteWidth + spacing) - totalWidth / 2;
-                GuiComponent.blit(poseStack, (int) xPos, y - 54, spriteX, spriteY, spriteWidth, spriteHeight, 100, 16);
-                //GuiComponent.blit(poseStack, x, y - 54, spriteX, spriteY, spriteWidth, spriteHeight, 100, 16);
             }
         }
-            //for (int i = 0; i < 10; i++)     {
-            //    GuiComponent.blit(poseStack, x + (i * 9), y - 54, 0, 3, 15, 12, 9, 16);
-            //}
-        });
-
-
+    });
 }
 
 
--- src/main/java/net/epilekt/pantheon_of_doom/event/ClientEvents.java
+++ src/main/java/net/epilekt/pantheon_of_doom/event/ClientEvents.java
@@ -2,41 +2,30 @@ package net.epilekt.pantheon_of_doom.event;
 
 import net.epilekt.pantheon_of_doom.PantheonOfDoom;
 import net.epilekt.pantheon_of_doom.client.PlayerHp;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
-import net.minecraftforge.client.gui.overlay.IGuiOverlay;
-import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.client.event.RenderGuiOverlayEvent;
+import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-//import static net.epilekt.pantheon_of_doom.client.PlayerHp.modifyPlayerHealth;
-
 public class ClientEvents {
 
-
-    //@SubscribeEvent
-    //public static void onPlayerHealthChanged(LivingHurtEvent event) {
-    //    LivingEntity player = event.getEntity();
-    //    if (player instanceof Player) {
-    //        modifyPlayerHealth(player, 100f);
-    //    }
-    //}
     @Mod.EventBusSubscriber(modid = PantheonOfDoom.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class ClientModBusEvents1 {
+    public static class RpgHUDs {
         @SubscribeEvent
-        public static void registerGuiOverlays(RegisterGuiOverlaysEvent event) {
-            event.registerAboveAll("base", PlayerHp.BASE_HUD);
-        }
-    }
-    @Mod.EventBusSubscriber(modid = PantheonOfDoom.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class ClientModBusEvents2 {
-        @SubscribeEvent
-        public static void registerGuiOverlays(RegisterGuiOverlaysEvent event) {
+        public static void renderGuiOverlays(RegisterGuiOverlaysEvent event) {
+            event.registerAboveAll("base", PlayerHp.BASE_BAR);
             event.registerAboveAll("health", PlayerHp.HEALTH_HUD);
         }
     }
-
+    @SubscribeEvent
+    public static void renderVanillaHUDs(RenderGuiOverlayEvent.Pre event) {
+        if (VanillaGuiOverlay.PLAYER_HEALTH.type() == event.getOverlay()) {
+            event.setCanceled(true);
+        }
+        if (VanillaGuiOverlay.ARMOR_LEVEL.type() == event.getOverlay()) {
+            event.setCanceled(true);
+        }
+    }
 }
